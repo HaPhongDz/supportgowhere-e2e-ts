cc430f65301a3ac86c5ded0951e558a2ba2d7d6a
HaPhongDz <36852623+HaPhongDz@users.noreply.github.com>
2025-04-02 00:56:09 +0700

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ const config: PlaywrightTestConfig = {
     ['list']
   ],
   use: {
-    headless: false,
+    headless: true,
     viewport: { width: 1280, height: 720 },
     ignoreHTTPSErrors: true,
     video: 'retain-on-failure',
